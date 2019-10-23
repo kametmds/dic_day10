cc9cd06c9b31a318d186ce5d7418422079c658f0
NakatsuboYusuke <>
2019-10-23 15:46:25 +0900

    全ての変更をマージしました

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_23_053948) do
+ActiveRecord::Schema.define(version: 2019_10_23_061302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
     t.string "password_digest", null: false
-
-  create_table "blogs", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
